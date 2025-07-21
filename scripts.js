33c46c90b218a0aef97f4f6ae56648f959f1be73
Jayaseela Arasan <jayaseelaarasan.f@gmail.com>
2025-07-21 10:14:55 +0530

    added features: toggle play, updatebutton(for play and pause button change), skipping button

---
+++ scripts.js
@@ -0,0 +1,31 @@
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButton = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+//function for toggle play
+function togglePlay(){
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+}
+
+function updateButton(){
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+}
+
+function skip(){
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+//hooking up event listeners
+video.addEventListener('click', togglePlay)
+video.addEventListener('play', updateButton)
+video.addEventListener('pause', updateButton)
+
+toggle.addEventListener('click',togglePlay)
+
+skipButton.forEach(button => button.addEventListener('click', skip))
